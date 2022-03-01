aab6da3c6ad325df2bf5d26a1bdd699dbfcc8328
MaiHoa <hoa.le22@student.passerellesnumeriques.org>
2022-03-02 02:52:04 +0700

    fix error

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-// const fileUpload = require('express-fileupload');
-// const errorMiddleware = require('./middlewares/error');
+const fileUpload = require('express-fileupload');
+const errorMiddleware = require('./middlewares/error');
 
 const app = express();
 
@@ -15,17 +15,11 @@ if (process.env.NODE_ENV !== 'production') {
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(fileUpload());
+app.use(fileUpload());
 
-// const user = require('./routes/userRoute');
-// const product = require('./routes/productRoute');
-// const order = require('./routes/orderRoute');
-// const payment = require('./routes/paymentRoute');
+const user = require('./routes/userRoute');
 
-// app.use('/api/v1', user);
-// app.use('/api/v1', product);
-// app.use('/api/v1', order);
-// app.use('/api/v1', payment);
+app.use('/api/v1', user);
 
 // deployment
 __dirname = path.resolve();
@@ -42,6 +36,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // error middleware
-// app.use(errorMiddleware);
+app.use(errorMiddleware);
 
 module.exports = app;
